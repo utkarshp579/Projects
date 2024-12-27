c9c68e025bc2d64ae57468f600f82411581c22bf
Utkarsh <utkarsh@example.com>
2024-12-27 14:56:28 +0530

    Weather App is now moved to project folder

--- Weather App/script2.js
+++ Weather App/script2.js
@@ -12,49 +12,38 @@ let oldTab = userTab;
 const API_KEY = "2eae51938341b642d6c24dd00e35394e";
 oldTab.classList.add("current-tab");
 getfromSessionStorage();
-// ek kaam aur pending hi
-
-// console.log(sessionStorage);
 
 // check if coordinates are already present in session storage 
 function getfromSessionStorage(){
-    const localCoordinates = sessionStorage.getItem("user-coordinates");// session storage is a object , which stores session detail, that we get thorugh geological api
+    const localCoordinates = sessionStorage.getItem("user-coordinates");
     if(!localCoordinates){
-        // agar local coordinates nhi mile
         grantAccessContainer.classList.add("active");
     }
     else{
-        const coordinates = JSON.parse(localCoordinates);// coverts  json string into JSON object
+        const coordinates = JSON.parse(localCoordinates);
         fetchUserWeatherInfo(coordinates);
     }
 }
 
-async function fetchUserWeatherInfo(coordinates){// coordinates is a JSON object which wwe get after parsing json string.
+async function fetchUserWeatherInfo(coordinates){
     const {lat, lon} = coordinates;
-    // console.log(lat);// working
-    // make grantcontainer invisible
     grantAccessContainer.classList.remove("active");
-    // make loader visible
     loadingScreen.classList.add("active");
-
 
     // API call
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
-        const data = await response.json();// this line conerts into json object
-
-        loadingScreen.classList.remove("remove");
-        userInfoContainer.classList.add("active");
-        renderWeatherInfo(data);// data is JS object
-    } catch (err) {
-        //hw
+        const data = await response.json();
         loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    } catch (err) {
+        loadingScreen.classList.remove("active");
+        console.error("Error fetching user weather info:", err);
     }
 }
 
-function renderWeatherInfo(weatherInfo){// weather Info is JavaScript object , that we get through json string returned by promise.
-    //fistly, we have to fethc the elements 
-
+function renderWeatherInfo(weatherInfo){
     const cityName = document.querySelector("[data-cityName]");
     const countryIcon = document.querySelector("[data-countryIcon]");
     const desc = document.querySelector("[data-weatherDesc]");
@@ -64,94 +53,68 @@ function renderWeatherInfo(weatherInfo){// weather Info is JavaScript object , t
     const humidity = document.querySelector("[data-humidity]");
     const cloudiness = document.querySelector("[data-cloudiness]");
 
-    // console.log(weatherInfo);
-    // console.log( "hello");
-    // fetch values from weatherInfo object and put it UI elements
-    // 1 hr 16minconsole.log( "hello1");
-    cityName.innerText = weatherInfo?.name;// as in database after connverting given response into json we see that it is direct child of object
-    // console.log( "hello2");
-    countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;// optional chaining humhe error nhi deta hi agar object nhi hi to ye return krta hi ek undefined
-    // console.log( "hello3");
+    cityName.innerText = weatherInfo?.name;
+    countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
     desc.innerText = weatherInfo?.weather?.[0]?.description;
     weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-    // console.log( "hello4");
     temp.innerText = `${weatherInfo?.main?.temp} °C`;
     windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
     humidity.innerText = `${weatherInfo?.main?.humidity}%`;
-    cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;// paragraph ka attrributee hi , humara innertext
-    // console.log( "hello5");// working
+    cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
 }  
 
 function switchTab(newTab){
-    if(newTab != oldTab){// button ke UI me change kr diya
+    if(newTab != oldTab){
         oldTab.classList.remove("current-tab");
         oldTab = newTab;
         oldTab.classList.add("current-tab");
 
-        // tab ke UI me change krna
         if(!searchForm.classList.contains("active")) {
-            //kya search form wala container is invisible, if yes then make it visible
             userInfoContainer.classList.remove("active");
             grantAccessContainer.classList.remove("active");
             searchForm.classList.add("active");
         } 
         else{
-            // means pehle search tab pr tha , ab weather tab visible krna h
             searchForm.classList.remove("active");
             userInfoContainer.classList.remove("active");
-            getfromSessionStorage(); // getting current location from current session if stored
+            getfromSessionStorage();
         }
     }
-
 }
 
 userTab.addEventListener("click",()=>{
-    // pass userTab as input parameter 
     switchTab(userTab);
 });
 searchTab.addEventListener("click",()=>{
-    // pass userTab as input parameter 
-    console.log( "search1");
     switchTab(searchTab);
-    console.log( "search2");
 });
 
-// getLocation();
 const grantAccessButton = document.querySelector("[data-grantAccess]");
 grantAccessButton.addEventListener('click',getLocation);
 
 function getLocation(){
-    console.log("getLocation called."); // working
-    if(navigator.geolocation){// geolocation is a api , which have method to take location of user, so we checkingthat navigator is supporting geolocation api in device or not.
-        console.log("geoLocation exist");// working
+    if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition);
     }else{
-        // show alert 
         console.log("No geoLocation support");
     }
 }
 
-
-function showPosition(position) {// position attribute is passed defaulty after successful of method of getCurrentPosition() in navigator.geolocation
-
-    const userCoordinates = {// storing position
+function showPosition(position) {
+    const userCoordinates = {
         lat: position.coords.latitude,
         lon: position.coords.longitude,
     }
-    // console.log(lat);
-
-    // sessionstorage object ke ander store kr rhe h , to we need data in json string format
-    sessionStorage.setItem("user-coordinates", JSON.stringify(userCoordinates));// stringify , covets objects into string
+    sessionStorage.setItem("user-coordinates", JSON.stringify(userCoordinates));
+    loadingScreen.classList.remove("active");
     fetchUserWeatherInfo(userCoordinates);
-
 }
 
 const searchInput = document.querySelector("[data-searchInput]");
 
 searchForm.addEventListener("submit", (e) => {
-    // console.log("Searcha");// working
     e.preventDefault();
-    let cityName = searchInput.value;// as searchInput is input holder , so it have value
+    let cityName = searchInput.value;
 
     if(cityName === "")
         return;
@@ -174,6 +137,7 @@ async function fetchSearchWeatherInfo(city) {
         renderWeatherInfo(data);
     }
     catch(err) {
-        //hW
+        loadingScreen.classList.remove("active");
+        console.error("Error fetching search weather info:", err);
     }
 }
